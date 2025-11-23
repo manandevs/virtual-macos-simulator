cc8de75a52a5b808b3b5da384d9044407955915c
manandevs <abdulmanan.devs@gmail.com>
2025-11-23 10:09:04 +0500

    Initial commit: Add Virtual MacOS simulator with Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import { Navbar } from '@components';
 
 const App = () => {
   return (
     <React.Fragment>
       <Navbar />
+      {/* Your other app components go here */}
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { navLinks, navIcons } from "../constants/index"; // adjust path if needed
+import dayjs from "dayjs";
+import { navLinks, navIcons } from "../constants/index"; 
 
 const Navbar = () => {
+
   return (
     <nav className="w-full flex items-center justify-between px-6 py-3 backdrop-blur-md bg-white/20 fixed top-0 left-0 z-50">
-      
       {/* Logo */}
       <div className="flex items-center gap-2">
         <img src="/macbook.png" alt="logo" className="w-8 h-8" />
-        <span className="font-semibold text-gray-900">My Portfolio</span>
+        <span className="font-semibold text-gray-900">Virtual MacOS</span>
       </div>
 
       {/* Navigation Links */}
@@ -23,7 +23,7 @@ const Navbar = () => {
         ))}
       </ul>
 
-      {/* Right Icons */}
+      {/* Right Icons + Time */}
       <ul className="flex items-center gap-4">
         {navIcons.map((icon) => (
           <li key={icon.id}>
@@ -34,6 +34,11 @@ const Navbar = () => {
             />
           </li>
         ))}
+        <li className="ml-4 font-mono text-gray-800">
+          <time>
+            {dayjs().format("ddd MMM D h:mm A")}
+          </time>
+        </li>
       </ul>
     </nav>
   );
